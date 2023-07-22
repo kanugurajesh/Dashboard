90ca981bb95d4e0a0cb4232dc323489bacd6899a
Rajesh604 <kanugurajesh3@gmail.com>
2023-07-22 23:27:05 +0530

    adding all the data

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import { useState } from 'react'
 import { BellOutlined } from '@ant-design/icons'
+import styled from 'styled-components'
+
+const Div = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
 
 function App() {
   return (
     <>
       <header>
-        <div>
+        <Div>
           <BellOutlined style={{fontSize:'30px'}}/>
-        </div>
-
+        </Div>
       </header>
     </>
   )
