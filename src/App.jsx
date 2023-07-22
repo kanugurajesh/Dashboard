bcb5ed4e7741101367cf8d2059b49c3a0c796f28
Rajesh604 <kanugurajesh3@gmail.com>
2023-07-23 00:31:22 +0530

    adding all the data

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
         <BellOutlined style = {{fontSize: '26px'}} id='notification'/>
         </div>
         <div id="badge">
-          <Avatar alt="Remy Sharp" src="https://ik.imagekit.io/hbzknb1hm/rajesh1.jpeg?updatedAt=1687880887870" style={{width:'80px',height:'80px'}}/>
+          <Avatar alt="Remy Sharp" src="https://ik.imagekit.io/hbzknb1hm/rajesh1.jpeg?updatedAt=1687880887870" style={{width:'90px',height:'90px'}}/>
           <div>
             <h1>Hi Rajesh</h1>
             <p>Here's what's happening with your projects today</p>
