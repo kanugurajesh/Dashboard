67f4bffb9a7a80a6d415bea2b6acd6d4688c28d9
Rajesh604 <kanugurajesh3@gmail.com>
2023-07-23 19:29:47 +0530

    adding all the data

--- src/App.jsx
+++ src/App.jsx
@@ -103,7 +103,8 @@ function App() {
       </div>
       <footer>
         <div className={`footer-element ${index === 0 ? 'violet': ''}`} onClick={() => setIndex(0)}>
-          <img src="https://ik.imagekit.io/hbzknb1hm/home.png?updatedAt=1690089551208" alt="" />
+          {/* <img src="https://ik.imagekit.io/hbzknb1hm/home.png?updatedAt=1690089551208" alt="" /> */}
+          <img src="https://ik.imagekit.io/hbzknb1hm/home_1.png?updatedAt=1690120755550" alt="" />
         </div>
         <div className={`footer-element ${index === 1 ? 'violet': ''}`} onClick={() => setIndex(1)}>
           <img src="https://ik.imagekit.io/hbzknb1hm/letter1.png?updatedAt=1690089551603" alt="" />
