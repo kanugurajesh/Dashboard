a007820cfeb6cb580fec8c78110bf199149587a4
Rajesh604 <kanugurajesh3@gmail.com>
2023-07-23 19:34:37 +0530

    adding all the data

--- src/App.jsx
+++ src/App.jsx
@@ -103,20 +103,19 @@ function App() {
       </div>
       <footer>
         <div className={`footer-element ${index === 0 ? 'violet': ''}`} onClick={() => setIndex(0)}>
-          {/* <img src="https://ik.imagekit.io/hbzknb1hm/home.png?updatedAt=1690089551208" alt="" /> */}
           <img src="https://ik.imagekit.io/hbzknb1hm/home_1.png?updatedAt=1690120755550" alt="" />
         </div>
         <div className={`footer-element ${index === 1 ? 'violet': ''}`} onClick={() => setIndex(1)}>
-          <img src="https://ik.imagekit.io/hbzknb1hm/letter1.png?updatedAt=1690089551603" alt="" />
+          <img src="https://ik.imagekit.io/hbzknb1hm/letter.png?updatedAt=1690120983552" alt="" />
         </div>
         <div className={`footer-element ${index === 2 ? 'violet': ''}`} onClick={() => setIndex(2)}>
-        <img src="https://ik.imagekit.io/hbzknb1hm/letter2.png?updatedAt=1690089551592" alt="" />
+        <img src="https://ik.imagekit.io/hbzknb1hm/radio.png?updatedAt=1690120983463" alt="" />
         </div>
         <div className={`footer-element ${index === 3 ? 'violet': ''}`} onClick={() => setIndex(3)}>
-          <img src="https://ik.imagekit.io/hbzknb1hm/user.png?updatedAt=1690089551598" alt="" />
+          <img src="https://ik.imagekit.io/hbzknb1hm/user.png?updatedAt=1690120983785" alt="" />
         </div>
         <div className={`footer-element ${index === 4 ? 'violet': ''}`} onClick={() => setIndex(4)}>
-          <img src="https://ik.imagekit.io/hbzknb1hm/search.png?updatedAt=1690089551593" alt="" />
+          <img src="https://ik.imagekit.io/hbzknb1hm/search.png?updatedAt=1690120983773" alt="" />
         </div>
       </footer>
       </>  
